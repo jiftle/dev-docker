7ad1db6639469bfca39ec72137a6e394f7cc6daa
mac <mac@macbookdeMBP.lan>
2020-08-31 00:00:47 +0800

    ok

--- dev-env-gui-incr-test/sh/mac-exec.sh
+++ dev-env-gui-incr-test/sh/mac-exec.sh
@@ -10,15 +10,14 @@ cd ..
 # ------------ 引用脚本 --------------
 . ./conf.conf
 
-# ------------ 业务脚本 --------------
-# docker exec -it ${ContainerName} bash
 
 # ------- 处理GUI显示的问题,原理:x11共享 -------
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 
-# docker exec --user john --workdir /home/john -it ${DockerKey} zsh
 echo "  |--> 当前主机IP: $IP"
 xhost + $IP
+
+# -------- 附加 -------
 docker exec \
 -it \
 -w /home/john \
@@ -26,3 +25,12 @@ docker exec \
 -e DISPLAY=$IP:0 \
 ${ContainerName} \
 zsh
+
+# # -------- 附加 -------
+# docker exec \
+# -it \
+# -w /home/john \
+# -u john \
+# -e DISPLAY=$IP:0 \
+# ${ContainerName} \
+# zsh
--- dev-env-gui-incr-test/sh/mac-run.sh
+++ dev-env-gui-incr-test/sh/mac-run.sh
@@ -19,8 +19,6 @@ echo "------------- 运行[${DockerKey}:${DockerVer}]镜像的容器 -----------
 
 echo "---> 开始启动[${DockerKey}:${DockerVer}] Docker"
 
-# -e GDK_SCALE \
-# -e GDK_DPI_SCALE \
 # ----------- 运行容器 ------------
 docker run -itd \
 `# 注释请忽略，----------- 设置名称 -------------` \
@@ -32,15 +30,17 @@ docker run -itd \
 `# 注释请忽略，----------- 用户密钥 -------------` \
 -v ${HOME}/.ssh:/root/.ssh `# .ssh密钥` \
 `# 注释请忽略，----------- 端口映射 -------------` \
--p 9022:22 `# ssh端口` \
+-p 9022:22 `# 日志仓库，预览服务` \
 -p 9001:8001 `# 日志仓库，预览服务` \
 -p 9080:8080 `# web程序调试端口` \
 -h "${ContainerName}" `# 机器名` \
+--privileged \
 --restart=always `# 自启动，容易导致挂载的分区，晚于docker` \
 `# 注释请忽略，----------- 图形界面 -------------` \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -e DISPLAY=${HostIp}:0 \
 `# 镜像名` \
-${DockerKey}:${DockerVer}
+${DockerKey}:${DockerVer} \
+/sbin/init
 echo "---> 启动[${DockerKey}:${DockerVer}] Docker完毕"
 
